6a129e26fc383f696f56d53ba33aab74c8570862
Jihyeong Son <152352575+jhsonnn@users.noreply.github.com>
2024-01-24 13:11:29 +0900

    Update x만큼 간격이 있는 n개의 숫자.js

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
@@ -1,6 +1,8 @@
 function solution(x, n) {
     var answer = [];
     for(let i = 0; i < n; i++){
+        //등차수열 : an = a1 + (n-1)d;
+        //ai = x + ((i+i)-1)*x;
         answer.push(x + ((i+1)-1)*x);
     }
     return answer;
