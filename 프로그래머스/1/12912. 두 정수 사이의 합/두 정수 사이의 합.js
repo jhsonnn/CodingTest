4dd14bb388b1f31207553d4f50560d664409abd7
Jihyeong Son <152352575+jhsonnn@users.noreply.github.com>
2024-01-30 13:35:40 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.42 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,22 @@
+function solution(a, b) {
+    var answer = 0;
+    let arr = [];
+    let arrLength = Math.abs(a - b) + 1;
+
+    if(a === b){
+        arr[0] = a;
+    }else if(a-b<0){
+        arr[0] = a;
+        arr[arrLength-1] = b;
+    }else if(a-b>0){
+        arr[0] = b;
+        arr[arrLength-1] = a;
+    }
+
+    for(let i = 0; i < arrLength; i++){
+        arr[i] = arr[0]+i;
+        answer += arr[i];
+    }
+
+    return answer;
+}
