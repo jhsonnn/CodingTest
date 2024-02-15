f18db117a5ebb445073a13d557ca299578ace7e1
Jihyeong Son <152352575+jhsonnn@users.noreply.github.com>
2024-02-15 09:43:08 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,19 @@
+function solution(s) {
+    var answer = true;
+
+    if(s.length === 4 || s.length === 6){
+        for(const element of s){
+            let num = Number(element);
+            // 숫자로 바꿀 수 있는지 확인하는 함수
+            // 못바꾸면 true, 변환가능하면 false
+            if(isNaN(num)){
+                answer = false;
+                //break;
+            }
+        }
+    }else{
+        answer =  false;
+    }
+
+    return  answer;
+}
