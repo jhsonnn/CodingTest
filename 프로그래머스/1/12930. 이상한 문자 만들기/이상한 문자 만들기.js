119e5fb766be824092daaaa5069593f5c19af01b
Jihyeong Son <152352575+jhsonnn@users.noreply.github.com>
2024-02-20 14:35:17 +0900

    Update 이상한 문자 만들기.js

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,21 +1,23 @@
 function solution(s) {
     let answer = '';
-    let words = s.split(' ');
+    let str = '';
+    let start = 0;
 
-    for (let i = 0; i < words.length; i++) {
-        let word = words[i];
-        
-        for (let j = 0; j < word.length; j++) {
-            if (j % 2 === 0) {
-                answer += word[j].toUpperCase();
+    for(let i = start; i < s.length; i++){
+        if(s[i] !== ' '){
+            str += s[i];
+            // 공백 아닌 것들 대소문자 바꾸고 answer에 넣기
+            // 인덱스 짝홀수 판별이 아닌 단어 내 글자의 인덱스로 판별
+            if(str.length % 2 !== 0){
+                //str에 들어간 문자의 마지막 문자
+                answer += str.charAt(str.length -1).toUpperCase();
             } else {
-                answer += word[j].toLowerCase();
+                answer += str.charAt(str.length -1).toLowerCase();
             }
-        }
-
-        if (i < words.length - 1) {
-            // 마지막 단어가 아닌 경우에는 공백 추가
+        } else {
             answer += ' ';
+            str = '';
+            start = i+1;
         }
     }
 
